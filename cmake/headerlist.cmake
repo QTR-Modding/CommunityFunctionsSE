69f6ee3d9d76afab063721a26060c1e6d782b9b6
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2026-08-31 20:08:27 +0200

    Add scalable function registry

--- cmake/headerlist.cmake
+++ cmake/headerlist.cmake
@@ -1,3 +1,5 @@
 set(headers
     include/CommunityFunctionsSE/Functions.hpp
+    include/CommunityFunctionsSE/Functions/WouldBeStealing.hpp
+    include/CommunityFunctionsSE/Registration.hpp
 )
--- include/CommunityFunctionsSE/Functions.hpp
+++ include/CommunityFunctionsSE/Functions.hpp
@@ -1,36 +1,25 @@
 #pragma once
 
-#include "RE/Skyrim.h"
+#include "CommunityFunctionsSE/Registration.hpp"
+#include "CommunityFunctionsSE/Functions/WouldBeStealing.hpp"
 
 namespace CommunityFunctionsSE {
-    inline constexpr std::uint32_t kFunctionBase = 1000;
-    inline constexpr std::uint32_t kWouldBeStealing = kFunctionBase;
-
     namespace detail {
-        inline bool WouldBeStealing(RE::TESObjectREFR* a_subject, void* a_target, void*, double& a_result) {
-            const auto actor = a_subject ? a_subject->As<RE::Actor>() : nullptr;
-            const auto target = static_cast<RE::TESObjectREFR*>(a_target);
-            if (!actor || !target) {
-                return false;
-            }
-
-            a_result = actor->WouldBeStealing(target);
-            return true;
-        }
-
-        inline RE::SCRIPT_PARAMETER wouldBeStealingParameters[]{
-            { .paramName = "Object Reference", .paramType = RE::SCRIPT_PARAM_TYPE::kObjectRef }
+        inline constexpr std::array entries{
+            Normalize(WouldBeStealing::registration)
         };
 
-        inline const RE::SCRIPT_FUNCTION wouldBeStealing{
-            .functionName = "WouldBeStealing",
-            .numParams = static_cast<std::uint16_t>(std::size(wouldBeStealingParameters)),
-            .params = wouldBeStealingParameters,
-            .conditionFunction = WouldBeStealing
-        };
+        static_assert(IsValid(entries), "Invalid community function registration");
+
+        inline constexpr auto functions = BuildRegistry<GetLookupSize(entries)>(entries);
     }
 
-    [[nodiscard]] inline const RE::SCRIPT_FUNCTION* GetFunction(const std::uint32_t a_id) noexcept {
-        return a_id == kWouldBeStealing ? &detail::wouldBeStealing : nullptr;
+    [[nodiscard]] constexpr const RE::SCRIPT_FUNCTION* GetFunction(const std::uint32_t a_id) noexcept {
+        if (a_id < kFunctionBase) {
+            return nullptr;
+        }
+
+        const auto index = a_id - kFunctionBase;
+        return index < detail::functions.size() ? detail::functions[index] : nullptr;
     }
 }
