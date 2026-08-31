b8a052689104b36982368d4da463a319b5a76973
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2026-08-31 22:21:22 +0200

    Add public API header

--- cmake/headerlist.cmake
+++ cmake/headerlist.cmake
@@ -1,4 +1,5 @@
 set(headers
+    include/CommunityFunctionsSE/API.hpp
     include/CommunityFunctionsSE/Functions.hpp
     include/CommunityFunctionsSE/Functions/Quantumyilmaz/WouldBeStealing.hpp
     include/CommunityFunctionsSE/Registration.hpp
--- include/CommunityFunctionsSE/Functions.hpp
+++ include/CommunityFunctionsSE/Functions.hpp
@@ -13,13 +13,4 @@ namespace CommunityFunctionsSE {
 
         inline constexpr auto functions = BuildRegistry<GetLookupSize(entries)>(entries);
     }
-
-    [[nodiscard]] constexpr const RE::SCRIPT_FUNCTION* GetFunction(const std::uint32_t a_id) noexcept {
-        if (a_id < kFunctionBase) {
-            return nullptr;
-        }
-
-        const auto index = a_id - kFunctionBase;
-        return index < detail::functions.size() ? detail::functions[index] : nullptr;
-    }
 }
